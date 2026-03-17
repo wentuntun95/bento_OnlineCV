9fe86a2eb12eeb4be6200df86b85837d9cefb5ee
wentuntun95 <wentuntun95@gmail.com>
2026-03-17 10:22:27 +0800

    fix: enable ISR for sanity data updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getSiteData } from "@/lib/data";
 import ClientPage from "./ClientPage";
 
+export const revalidate = 60; // 开启 ISR，每 60 秒在后台重新验证并更新一次缓存
+
 export default async function Home() {
   const siteData = await getSiteData();
 
